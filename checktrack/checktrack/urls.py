f1abfd9a62a3d8a2d0ad10c214d5fa098b1f1cc8
rogevand <rvand516@gmail.com>
2017-09-29 12:29:05 -0400

    dope new homepage

--- checktrack/checktrack/urls.py
+++ checktrack/checktrack/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 import check
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^check/', include('check.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^main/', check.views.main, name='main'),
-]
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

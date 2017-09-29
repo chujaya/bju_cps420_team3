31bb7dfb718ca3157b0d1432dd5b902fd1d29901
rogevand <rvand516@gmail.com>
2017-09-29 11:27:27 -0400

    got some templates working

--- checktrack/checktrack/urls.py
+++ checktrack/checktrack/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import check
 
 urlpatterns = [
     url(r'^check/', include('check.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^main/', check.views.main, name='main'),
 ]
 

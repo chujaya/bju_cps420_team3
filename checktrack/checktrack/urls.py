407fc176692b2ceb8bf763693c6a0f57b805c327
rogevand <rvand516@gmail.com>
2017-10-24 10:15:46 -0400

    local

--- checktrack/checktrack/urls.py
+++ checktrack/checktrack/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 import check
 from django.conf import settings
-<<<<<<< HEAD
-=======
 from django.contrib.auth import views as auth_views
->>>>>>> 0cff3080d2f9aeba8b17c0a40fc7549e31141a90
 from django.conf.urls.static import static
 
 
@@ -15,12 +12,7 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^main/', check.views.main, name='main'),
-<<<<<<< HEAD
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-    url(r'^home/', check.views.home, name="home"),
+    url(r'^home/', check.views.home, name="home")
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
->>>>>>> 0cff3080d2f9aeba8b17c0a40fc7549e31141a90
